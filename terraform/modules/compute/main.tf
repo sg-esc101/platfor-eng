82275f92267173cb9a2dd53b57877f2b3f014dc7
simon g <freemeaws@outlook.com>
2025-09-08 03:41:27 +0530

    enhanced vm capabilities

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,3 +1,12 @@
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "${var.vm_name}-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
   location            = var.location
@@ -7,17 +16,42 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = var.resource_group_name
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
   location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.vm_name
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  size                            = "Standard_B1s"
+
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
   disable_password_authentication = false
-  admin_password      = var.admin_password   
 
   network_interface_ids = [azurerm_network_interface.nic.id]
 
@@ -32,4 +66,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
+
+output "vm_public_ip" {
+  description = "Public IP of the VM for SSH access"
+  value       = azurerm_public_ip.vm_public_ip.ip_address
 }
