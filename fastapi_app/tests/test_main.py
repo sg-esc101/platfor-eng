f3bdaa399ee6ece987d8d00d2429e4eedc65727b
simon g <freemeaws@outlook.com>
2025-09-08 19:24:08 +0530

    fixed ci.yaml

--- fastapi_app/tests/test_main.py
+++ fastapi_app/tests/test_main.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 # Add the parent directory of 'fastapi_app' to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append("/home/hacker/platfor-eng/fastapi_app")
 
 from fastapi_app.main import app
 from fastapi.testclient import TestClient
