7fac4634892c5a901c0b00e9a26cc2c9d9bf445f
simon g <freemeaws@outlook.com>
2025-09-08 21:05:08 +0530

    added cd part

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -28,6 +28,34 @@ resource "azurerm_network_security_rule" "ssh_rule" {
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
+resource "azurerm_network_security_rule" "http_rule" {
+  name                        = "HTTP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "https_rule" {
+  name                        = "HTTPS"
+  priority                    = 1003
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
