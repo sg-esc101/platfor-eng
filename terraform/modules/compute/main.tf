0947dc8379b58d385e4a6ab0b178c78329da3bc2
simon g <freemeaws@outlook.com>
2025-09-08 22:01:02 +0530

    edited compute module

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -56,6 +56,21 @@ resource "azurerm_network_security_rule" "https_rule" {
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
+resource "azurerm_network_security_rule" "nodeport_rule" {
+  name                        = "AllowNodePorts"
+  priority                    = 1004
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "30000-32767"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
